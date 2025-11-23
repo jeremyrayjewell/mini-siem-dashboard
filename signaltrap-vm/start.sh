e35b44eb6054ac05efbb7719e38d65cfecd043ca
Jeremy Jewell <jeremyrayjewell@gmail.com>
2025-11-23 09:04:53 -0500

    FIX: Keep TCP listener process alive with infinite loop

--- signaltrap-vm/start.sh
+++ signaltrap-vm/start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Start TCP listeners in background using Python directly
-python3 -c "from server import start_tcp_listeners; start_tcp_listeners()" &
+# Start TCP listeners in background - keep process alive with infinite loop
+python3 -c "
+from server import start_tcp_listeners
+import time
+start_tcp_listeners()
+print('[START.SH] TCP listeners running, keeping process alive...')
+while True:
+    time.sleep(3600)
+" &
 
 # Give listeners time to bind
-sleep 2
+sleep 3
 
 # Start gunicorn in foreground
 exec gunicorn --config gunicorn.conf.py --bind 0.0.0.0:8080 --workers 1 server:app
