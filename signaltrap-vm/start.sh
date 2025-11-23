2dee5f3bd829bf63c0b9202b4fc33decb6748e58
Jeremy Jewell <jeremyrayjewell@gmail.com>
2025-11-23 09:58:39 -0500

    FIX: Ignore signals in background TCP listener process

--- signaltrap-vm/start.sh
+++ signaltrap-vm/start.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # Start TCP listeners in separate background process with unbuffered output
-python3 -u -c "
+# Use nohup to prevent signals from parent shell
+nohup python3 -u -c "
 from server import start_tcp_listeners
 import time
 import signal
 import sys
 
-# Handle signals gracefully
-def signal_handler(sig, frame):
-    print('[TCP LISTENERS] Shutting down...')
-    sys.exit(0)
-
-signal.signal(signal.SIGTERM, signal_handler)
-signal.signal(signal.SIGINT, signal_handler)
+# Ignore common termination signals - we only want explicit shutdown
+signal.signal(signal.SIGTERM, signal.SIG_IGN)
+signal.signal(signal.SIGINT, signal.SIG_IGN)
+signal.signal(signal.SIGHUP, signal.SIG_IGN)
 
 print('[TCP LISTENERS] Starting all TCP listeners...')
 start_tcp_listeners()
@@ -22,7 +20,7 @@ print('[TCP LISTENERS] All listeners started, keeping process alive...')
 # Keep process alive
 while True:
     time.sleep(60)
-" &
+" > /tmp/tcp_listeners.log 2>&1 &
 
 LISTENER_PID=$!
 echo "[START.SH] TCP listeners started in background (PID: $LISTENER_PID)"
